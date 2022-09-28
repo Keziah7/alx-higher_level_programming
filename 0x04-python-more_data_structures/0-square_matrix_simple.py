db4ad1f8b5f0dcc1bb622ced29f4596e2c663343
Keziah7 <keziellove5@gmail.com>
2022-09-28 12:15:55 +0000

    0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-return [list(map((lambda x: x * x), elm)) for elm in matrix]
+if matrix is None:
+return None
+return list(list(map(lambda x: x ** 2, y)) for y in matrix)
