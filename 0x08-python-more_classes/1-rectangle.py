60cf2cf873a62decf3cf44a171928a6fa372da6d
Keziah7 <ongangikeziah@gmail.com>
2022-10-17 19:17:48 -0700

    1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,32 +1,39 @@
 #!/usr/bin/python3
-"""Contains the rectangle class"""
+"""
+Class Rectangle: Defines a Rectangle
+"""
+
+
 class Rectangle:
-    """A rectangle class"""
+    """ class that defines a Rectangle with attributes """
     def __init__(self, width=0, height=0):
-        """Rectangle initializor"""
         self.width = width
         self.height = height
-        @property
-        def width(self):
-            """Width property function"""
-            return self.__width
-        @width.setter
-        def width(self, width):
-            """Width setter"""
-            if type(width) is not int:
-                raise TypeError("width must be an integer")
-            if width < 0:
-                raise ValueError("width must be >= 0")
-            self.__width = width
-        @property
-        def height(self):
-            """Height property function"""
-            return self.__height
-        @height.setter
-        def height(self, height):
-            """Height setter"""
-            if type(height) is not int:
-                raise TypeError("height must be an integer")
-            if height < 0:
-                raise ValueError("height must be >= 0")
-            self.__height = height
+
+    @property
+    def width(self):
+        """ Retrieve the width of rectangle """
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        """ set passed private attribue of width """
+        if not (isinstance(value, int)):
+            raise TypeError("width must be an integer")
+        elif (value < 0):
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """ Retrieve the height of rectangle """
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        """ set passed private attribute of height """
+        if not (isinstance(value, int)):
+            raise TypeError("height must be an integer")
+        elif (value < 0):
+            raise ValueError("height must be >= 0")
+        self.__height = value
