52b2f84d102ba37986a57fcfb64eae5987c027c3
Keziah7 <102479013+Keziah7@users.noreply.github.com>
2022-10-25 21:38:33 +0300

    Update 11-student.py

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,34 +1,27 @@
 #!/usr/bin/python3
-"""
-Contains the clas "Student"
-"""
+""" Program that define a Student with filter """
 
 
 class Student:
-    """Representation of a student"""
+    """ class Student that defines a student """
     def __init__(self, first_name, last_name, age):
-        """Initializes the student"""
+        """ Constructor """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """returns a dictionary representation of a Student instance
-        with specified attributes"""
-        if attrs is None:
-            return self.__dict__
-        new_dict = {}
-        for a in attrs:
-            try:
-                new_dict[a] = self.__dict__[a]
-            except:
-                pass
-        return new_dict
+        """ retrieves a dictionary representation of a Student instance """
+        dic = {}
+        if (type(attrs) is not list):
+            return (self.__dict__)
+        else:
+            for i in attrs:
+                if (i in self.__dict__):
+                    dic[i] = self.__dict__[i]
+            return (dic)
 
     def reload_from_json(self, json):
-        """replaces all attributes of the Student instance"""
-        for key in json:
-            try:
-                setattr(self, key, json[key])
-            except:
-                pass
+        """ replaces all attributes of the Student instance """
+        for i in json:
+            self.__dict__[i] = json[i]
