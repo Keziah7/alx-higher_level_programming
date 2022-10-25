089b46c7d79877287d6b1870f1a9ea8c31ea2c95
Keziah7 <102479013+Keziah7@users.noreply.github.com>
2022-10-25 21:40:18 +0300

    Update 10-student.py

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,26 +1,22 @@
 #!/usr/bin/python3
-"""
-Contains the clas "Student"
-"""
+""" Program that define a Student with filter """
 
 
 class Student:
-    """Representation of a student"""
+    """ class Student that defines a student """
     def __init__(self, first_name, last_name, age):
-        """Initializes the student"""
+        """ Constructor """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """returns a dictionary representation of a Student instance
-        with specified attributes"""
-        if attrs is None:
-            return self.__dict__
-        new_dict = {}
-        for a in attrs:
-            try:
-                new_dict[a] = self.__dict__[a]
-            except:
-                pass
-        return new_dict
+        """ retrieves a dictionary representation of a Student instance """
+        dic = {}
+        if (type(attrs) is not list):
+            return (self.__dict__)
+        else:
+            for i in attrs:
+                if (i in self.__dict__):
+                    dic[i] = self.__dict__[i]
+            return (dic)
