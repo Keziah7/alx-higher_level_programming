2a3f5f7c030a3cbf7bafd54b476dd96d9dabbce9
Keziah7 <102479013+Keziah7@users.noreply.github.com>
2022-12-21 04:23:04 +0300

    Update 100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,11 +1,6 @@
 #!/usr/bin/node
-// imports array from data file; creates new array with map; prints both
 const list = require('./100-data').list;
-
 console.log(list);
-if (Array.isArray(list) && list.every(elem => typeof elem === 'number')) {
-  const newList = list.map((value, index) => value * index);
-  console.log(newList);
-} else {
-  console.log(undefined);
-}
+let num = 0;
+const map1 = list.map(x => x * num++);
+console.log(map1);
